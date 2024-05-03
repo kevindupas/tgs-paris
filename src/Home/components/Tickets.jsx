ab1be3430b41e8cc57092c355c99057bfb3f1287
Kévin Dupas <38907117+kevindupas@users.noreply.github.com>
2024-05-03 16:56:47 +0200

    Update Tickets.jsx

--- src/Home/components/Tickets.jsx
+++ src/Home/components/Tickets.jsx
@@ -72,7 +72,7 @@ export default function Tickets() {
 
                     <Fade delay={500} direction="up" triggerOnce>
                         <div
-                            className={`mt-12 space-y-4 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-${prices.length} place-items-center`}
+                            className="mt-12 space-y-4 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-5 place-items-center"
                         >
                             {
                                 prices && (
