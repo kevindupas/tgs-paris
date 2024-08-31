3a7f0b799b5a1595c6a0fff0eb584ed23271e946
Kévin Dupas <38907117+kevindupas@users.noreply.github.com>
2024-09-01 00:31:25 +0200

    fix blur

--- src/components/CategoryImage.jsx
+++ src/components/CategoryImage.jsx
@@ -1,67 +1,73 @@
-/* eslint-disable no-console */
-/* eslint-disable no-shadow */
-import React, { useEffect, useState } from 'react';
-import { Fade } from 'react-awesome-reveal';
-import { SALON_ID, URL } from '../utils/config';
-import Loader from './Loader';
+import React, { useEffect, useState } from "react";
+import { Fade } from "react-awesome-reveal";
+import { SALON_ID, URL } from "../utils/config";
+import Loader from "./Loader";
 
 export default function CategoryImage({ categoryId, categoryName }) {
-    const [error, setError] = useState(null);
-    const [category, setCategory] = useState([]);
-    useEffect(() => {
-        const options = {
-            method:  'GET',
-            mode:    'cors',
-            cache:   'default',
-            headers: {
-                Accept:         'application/json',
-                'Content-Type': 'application/json',
-            },
-        };
-        fetch(`${URL}/api/salon/${SALON_ID}/categories/${categoryId}`, options)
-            .then((res) => res.json())
-            .then(
-                (data) => {
-                    setCategory(data.data);
-                },
-                (error) => {
-                    setError(error);
-                },
-            );
-    }, [categoryId, categoryName]);
+  const [error, setError] = useState(null);
+  const [category, setCategory] = useState([]);
 
-    if (error) {
-        return (
-            <div>
-                Error:
-                {error.message}
-            </div>
-        );
-    }
-    if (!category || Object.entries(category).length === 0) {
-        return (
-            <Loader />
-        );
-    }
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      mode: "cors",
+      cache: "default",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    };
+    fetch(`${URL}/api/salon/${SALON_ID}/categories/${categoryId}`, options)
+      .then((res) => res.json())
+      .then(
+        (data) => {
+          setCategory(data.data);
+        },
+        (error) => {
+          setError(error);
+        }
+      );
+  }, [categoryId, categoryName]);
 
+  if (error) {
     return (
-        <section className="relative h-[calc(100vh-100px)] overflow-hidden">
-            <div className="absolute bottom-0 left-0 m-auto right-0 top-0">
-                <div className="w-full h-full relative overflow-hidden">
-                    {category.id === categoryId ? (
-                        <img src={URL + category.image} alt="" className="w-full h-full object-cover blur-[3px] lg:blur-none" loading="lazy" />
-
-                    )
-                        : <Loader />}
-                </div>
-            </div>
-
-            <div className="relative z-10 pb-[40px] lg:pb-[69px] max-w-[1200px] w-[calc(100%-30px)] mx-auto h-full justify-end flex-col flex">
-                <Fade delay={500} direction="up" triggerOnce>
-                    <h1 className="font-microgramma text-white text-[30px] lg:text-[96px] tracking-[0.566667px] leading-[.95] uppercase">{categoryName}</h1>
-                </Fade>
-            </div>
-
-        </section>
+      <div>
+        Error:
+        {error.message}
+      </div>
     );
+  }
+  if (!category || Object.entries(category).length === 0) {
+    return <Loader />;
+  }
+
+  return (
+    <section className="relative h-[calc(100vh-100px)] overflow-hidden">
+      <div className="absolute bottom-0 left-0 m-auto right-0 top-0">
+        <div className="w-full h-full relative overflow-hidden">
+          {category.id === categoryId ? (
+            <img
+              src={URL + category.image}
+              alt=""
+              className="w-full h-full object-cover lg:blur-none"
+              loading="lazy"
+            />
+          ) : (
+            <Loader />
+          )}
+
+          {/* Ajout du dégradé en bas de l'image */}
+          <div className="absolute bottom-0 left-0 right-0 h-32 z-10 bg-gradient-to-t from-black to-transparent pointer-events-none"></div>
+        </div>
+      </div>
+
+      <div className="relative z-10 pb-[40px] lg:pb-[69px] max-w-[1200px] w-[calc(100%-30px)] mx-auto h-full justify-end flex-col flex">
+        <Fade delay={500} direction="up" triggerOnce>
+          <h1 className="font-microgramma text-white text-[30px] lg:text-[96px] tracking-[0.566667px] leading-[.95] uppercase">
+            {categoryName}
+          </h1>
+        </Fade>
+      </div>
+    </section>
+  );
 }
