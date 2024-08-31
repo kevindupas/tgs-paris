406eeb6861d8b3c412b5a470d4cd2247fbc31de0
Kévin Dupas <38907117+kevindupas@users.noreply.github.com>
2024-08-31 22:43:20 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    build:   {
-        chunkSizeWarningLimit: 1600,
-        commonjsOptions:       {
-            include:      'node_modules/**',
-            namedExports: {
-                'node_modules/react/index.js': [
-                    'createContext',
-                ],
-            },
-        },
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+    commonjsOptions: {
+      include: "node_modules/**",
+      namedExports: {
+        "node_modules/react/index.js": ["createContext"],
+      },
     },
+    rollupOptions: {
+      external: ["react-ga4"],
+    },
+  },
 });
