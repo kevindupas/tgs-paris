09da428e80737c0c894e75228f23a01e74623b74
Kévin Dupas <dupas.kevin@gmail.com>
2024-09-01 21:14:50 +0200

    Update VideoMobile.jsx

--- src/Home/components/VideoMobile.jsx
+++ src/Home/components/VideoMobile.jsx
@@ -1,54 +1,53 @@
 /* eslint-disable react/no-danger */
 /* eslint-disable jsx-a11y/alt-text */
-import React from 'react';
-import { useSettings } from '../../context/ConfigurationContext';
+import React from "react";
+import { useSettings } from "../../context/ConfigurationContext";
 
 export default function Video() {
-    const { config } = useSettings();
+  const { config } = useSettings();
 
-    return (
-        <section className="relative h-[calc(100vh-80px)]">
-            <div className="relative z-10 max-w-[1200px] mx-auto h-full content-end flex items-center flex-row-reverse">
-                <blockquote className="text-white font-heading_bold tracking-[0.566667px] leading-[.95] indent-0 uppercase text-4xl border-l-[15px] border-[#E97665] pl-5 py-4 relative">
-                    <span
-                        className="drop-shadow-2xl"
-                        style={{
-                            transform:  'translate(0px, 0px)',
-                            opacity:    '1',
-                            visibility: 'inherit',
-                        }}
-                    >
-                        {config.event_date}
-                    </span>
-                </blockquote>
+  return (
+    <section className="relative h-[calc(100vh-80px)]">
+      <div className="relative z-20 max-w-[1200px] mx-auto h-full content-end flex items-center flex-row-reverse">
+        <blockquote className="text-white font-heading_bold tracking-[0.566667px] leading-[.95] indent-0 uppercase text-4xl border-l-[15px] border-[#E97665] pl-5 py-4 relative">
+          <span
+            className="drop-shadow-2xl"
+            style={{
+              transform: "translate(0px, 0px)",
+              opacity: "1",
+              visibility: "inherit",
+            }}
+          >
+            {config.event_date}
+          </span>
+        </blockquote>
 
-                <div className="absolute w-20 left-5 bottom-[-53px] z-10">
-                    <img
-                        style={{
-                            transform:  'translate(0px, 0px)',
-                            opacity:    '1',
-                            visibility: 'inherit',
-                        }}
-                        src="/dist/images/icons-test.png"
-                        alt=""
-                    />
-                </div>
-            </div>
-            <div
-                className="absolute bottom-0 left-0 mx-auto top-0 md:right-[calc(50%-300px)] lg:right-[calc(50%-516px)]"
-            >
-                <div className="relative h-full w-full blur-[4px]">
-                    <video
-                        loop
-                        muted
-                        autoPlay
-                        playsInline
-                        className="object-cover h-full w-full pt-[5em]"
-                    >
-                        <source src="/dist/video/paris_manga.mp4" type="video/mp4" />
-                    </video>
-                </div>
-            </div>
-        </section>
-    );
+        <div className="absolute w-20 left-5 bottom-[-53px] z-20">
+          <img
+            style={{
+              transform: "translate(0px, 0px)",
+              opacity: "1",
+              visibility: "inherit",
+            }}
+            src="/dist/images/icons-test.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="absolute bottom-0 left-0 mx-auto top-0 md:right-[calc(50%-300px)] lg:right-[calc(50%-516px)] z-10">
+        <div className="relative h-full w-full">
+          <div className="absolute top-0 left-0 h-full w-full bg-black opacity-50 z-10"></div>
+          <video
+            loop
+            muted
+            autoPlay
+            playsInline
+            className="object-cover h-full w-full pt-[5em] z-0"
+          >
+            <source src="/dist/video/paris_manga.mp4" type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    </section>
+  );
 }
