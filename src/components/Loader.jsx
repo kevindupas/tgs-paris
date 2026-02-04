2cfc3a6996f389fe154e982e1ae5adc7903a39e4
Kévin Dupas <38907117+kevindupas@users.noreply.github.com>
2026-02-04 08:24:16 +0100

    update logo

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { useSettings } from "../context/ConfigurationContext";
 
 function Loader() {
-    return (
-        <div>
-
-            <div className="fixed w-screen h-screen bg-[url('/dist/images/pattern-dark.jpeg')] flex z-50 inset-0 flex-col items-center justify-center">
-                <img className="w-96 animate-pulse" src="../dist/images/logo.png" alt="logo" />
-            </div>
-
-        </div>
-    );
+  const { config } = useSettings();
+  return (
+    <div>
+      <div className="fixed w-screen h-screen bg-[url('/dist/images/pattern-dark.jpeg')] flex z-50 inset-0 flex-col items-center justify-center">
+        <img
+          className="w-96 animate-pulse"
+          src={config.logo ?? "../dist/images/logo.png"}
+          alt="logo"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Loader;
