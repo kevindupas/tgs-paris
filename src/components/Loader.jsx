b6509a53d731fffd79ca24317681fb4a72f7b225
Kévin Dupas <38907117+kevindupas@users.noreply.github.com>
2026-02-06 11:55:41 +0100

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,15 +1,10 @@
-import React, { useContext } from "react";
-import { useSettings } from "../context/ConfigurationContext";
-import { URL } from "../utils/config";
-
 function Loader() {
-  const { config } = useSettings();
   return (
     <div>
       <div className="fixed w-screen h-screen bg-[url('/dist/images/pattern-dark.jpeg')] flex z-50 inset-0 flex-col items-center justify-center">
         <img
           className="w-96 animate-pulse"
-          src={URL + config.logo || "/dist/images/logo.png"}
+          src="/dist/images/logo.png"
           alt="logo"
         />
       </div>
